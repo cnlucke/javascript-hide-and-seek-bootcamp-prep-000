19ee785a0c4def0ff7f1c6fc73bc2a9b11b0fc3d
Cristy Lucke <cristy.lucke@gmail.com>
2017-10-03 14:10:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+//  1) index getFirstSelector(selector) returns the first element that matches the selector:
+// Define a function getFirstSelector(selector), which accepts a selector and returns the first element that matches.
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+//  2) index nestedTarget() pulls a .target out of #nested:
+
+//  3) index deepestChild() returns the most deeply nested child in #grand-node:
+
+//  4) index increaseRankBy(n) increases ranks in .ranked-list by n:
