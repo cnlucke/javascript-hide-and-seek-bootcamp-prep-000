0155da4b07417a9ac39af0f960c9e67e36ab1a9e
Cristy Lucke <cristy.lucke@gmail.com>
2017-10-03 14:30:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function getFirstSelector(selector) {
 //  2) index nestedTarget() pulls a .target out of #nested:
 //Define a function nestedTarget() that pulls a .target out of #nested (# is used for IDs in selectors — but you knew that because you read the docs, right? :) ). (Note that in index.html #nested and .target just happen to be divs. This method should work with arbitrary elements.)
 function nestedTarget() {
-  return document.querySelector('div.nested div.target')
+  return document.getElementById('nested').querySelector('div.target')
 }
 
 //  3) index deepestChild() returns the most deeply nested child in #grand-node:
